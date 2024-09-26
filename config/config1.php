6eb8441a2991e0c3e43cbec2dccc422ff5a093a4
roliberyucra <yucracuroroliber@gmail.com>
2024-09-26 09:32:44 -0500

    historial de cambios

---
+++ config/config1.php
@@ -0,0 +1,9 @@
+<?php
+    const BD_HOST = 'localhost';
+    const BD_NAME = '';
+    const BD_USER = '';
+    const BD_PASSWORD = '';
+
+    // Ingresar el link(enlace) del proyecto..
+    const BASE_URL = '';
+?>
