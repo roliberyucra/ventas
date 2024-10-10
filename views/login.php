de2c95e241ec95812019a255cf93b4fef563746f
roliberyucra <yucracuroroliber@gmail.com>
2024-10-10 11:41:13 -0500

    update_login7.0

--- views/login.php
+++ views/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo BASE_URL ?>/views/plantilla/css/style.css">
-    <link rel="icon" href="../img/loguito.png" type="image/x-icon">
+    <link rel="icon" href="./views/plantilla/img/loguito.png" type="image/x-icon">
     <title>Login</title>
 </head>
 <body>
