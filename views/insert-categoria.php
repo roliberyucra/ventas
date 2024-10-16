e272d0bf47503f5feb9c1d77105ae8f914555743
roliberyucra <yucracuroroliber@gmail.com>
2024-10-16 16:24:35 -0500

    update_form_compra

--- views/insert-categoria.php
+++ views/insert-categoria.php
@@ -3,12 +3,12 @@
         <center><h3><b>Formulario de registro de categoria</b></h3></center>
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Nombre de categoria: </label>
-        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre*" name="nombreCat">
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre*" name="nombreCategoria">
     </div>
 
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Detalle de la categoria: </label>
-        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Detalles*" name="detalleCat">
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Detalles*" name="detalleCategoria">
     </div>
     <center>
         <button type="submit" class="btn btn-danger">Registrar</button>
