1b106c74ea890efddb139181bb5a32dbfd7c1b99
roliberyucra <yucracuroroliber@gmail.com>
2024-10-16 05:20:11 -0500

    update_formulario

--- views/insert-categoria.php
+++ views/insert-categoria.php
@@ -1,4 +1,4 @@
-<div id="contenido">
+<div id="form-insert-control">
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Email address</label>
         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
