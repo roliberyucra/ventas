0d86b128ba7312c30dfa96fae7cce8b655266b77
roliberyucra <yucracuroroliber@gmail.com>
2024-10-16 05:32:06 -0500

    Update_forms

--- views/insert-categoria.php
+++ views/insert-categoria.php
@@ -1,15 +1,17 @@
 <div id="form-insert-control">
     <form action="">
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <label for="exampleFormControlInput1" class="form-label">Nombre de categoria: </label>
         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
     </div>
+    <!--
     <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <label for="exampleFormControlTextarea1" class="form-label">TextArea</label>
         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
     </div>
+    -->
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <label for="exampleFormControlInput1" class="form-label">Detalles de la categoria: </label>
         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
     </div>
     </form>
