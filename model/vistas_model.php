e0ec8a910d29a7b153dea00e63d65f373104af6b
roliberyucra <yucracuroroliber@gmail.com>
2024-10-17 10:59:44 -0500

    update-vistas-model

--- model/vistas_model.php
+++ model/vistas_model.php
@@ -1,7 +1,7 @@
 <?php
     class vistaModelo{
         protected static function obtener_vista($vista){
-            $nombres_permitidos = ['panel-administracion', 'view-categoria', 'view-compra', 'update-categoria', 'update-compra', 'update-detalle-venta', 'update-pago', 'update-persona', 'update-producto', 'update-sesion', 'update-venta', 'insert-categoria', 'insert-compra', 'insert-detalle-venta', 'insert-pago', 'insert-persona', 'insert-producto', 'insert-sesion', 'insert-venta', 'usuario', 'producto', 'descripcion', 'carrito', 'contactos', 'perfil', 'p-informativas', 'computadoras', 'laptops', 'impresoras', 'celulares', 'camaras', 'compras'];
+            $nombres_permitidos = ['panel-administracion', 'view-categoria', 'view-compra', 'view-detalle-venta', 'view-pago', 'view-persona', 'view-producto', 'view-sesion', 'view-venta', 'update-categoria', 'update-compra', 'update-detalle-venta', 'update-pago', 'update-persona', 'update-producto', 'update-sesion', 'update-venta', 'insert-categoria', 'insert-compra', 'insert-detalle-venta', 'insert-pago', 'insert-persona', 'insert-producto', 'insert-sesion', 'insert-venta', 'usuario', 'producto', 'descripcion', 'carrito', 'contactos', 'perfil', 'p-informativas', 'computadoras', 'laptops', 'impresoras', 'celulares', 'camaras', 'compras'];
             if (in_array($vista, $nombres_permitidos)) {
                 if (is_file("./views/".$vista.".php")) {
                     $contenido = "./views/".$vista.".php";
