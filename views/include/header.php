a24075508f83d35ffcead465eb67a0b80878a00b
roliberyucra <yucracuroroliber@gmail.com>
2024-10-17 17:55:37 -0500

    1234

--- views/include/header.php
+++ views/include/header.php
@@ -23,7 +23,13 @@
                 <div class="bloque1">
                     <a href="../index.html"><img src="./views/plantilla/img/letralogo.png" alt="localizacion" width="140px"></a>
                 </div>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -37,6 +43,9 @@
         </li>
       </ul>
     </div>
+  </div>
+</nav>
+
             </div>
             <div class="bloque_menu">
                 <form class="d-flex" role="search">
