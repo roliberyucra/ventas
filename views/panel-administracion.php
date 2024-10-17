fa65ff343f0ad498d82852076c5f980e47c6ac76
roliberyucra <yucracuroroliber@gmail.com>
2024-10-16 21:49:01 -0500

    update_linkeo

--- views/panel-administracion.php
+++ views/panel-administracion.php
@@ -2,7 +2,7 @@
     <form action="">
         <center><h3><b>BIENVENIDO al Panel de Administración</b></h3></center>
     <center>
-        <button type="button" onclick="location.href='view-categoria.php'" class="btn btn-danger">Ver Categorias</button>
+        <button type="button" href="<?php echo BASE_URL ?>/view-categoria" class="btn btn-danger">Ver Categorias</button>
         <button type="button" href="<?php echo BASE_URL ?>/insert-categoria" class="btn btn-light">Registrar categoria</button>
     </center>
     </form>
