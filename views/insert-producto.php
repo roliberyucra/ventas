45f4d6fbcdffa36f9a59c70afeea80b490624c40
roliberyucra <yucracuroroliber@gmail.com>
2024-10-17 12:03:17 -0500

    update-prueba

--- views/insert-producto.php
+++ views/insert-producto.php
@@ -27,41 +27,42 @@
     </div>
 
     <div class="mb-3">
-        <label for="fechaVenc" class="form-label">Fecha vencimiento: </label>
-        <input type="date" class="form-control" id="fechaVenc" placeholder="Fecha de vencimiento*" name="fechaVencimiento" not required>
+        <label for="fechaVencimiento" class="form-label">Fecha vencimiento: </label>
+        <input type="date" class="form-control" id="fechaVencimiento" placeholder="Fecha de vencimiento*" name="fechaVencimiento" not required>
     </div>
 
     <div class="mb-3">
         <label for="img1" class="form-label">Imagen de producto 1: </label>
-        <input type="file" class="form-control" id="img1" name="imagen_1">
+        <input type="file" class="form-control" id="imagen1" name="imagen_1">
     </div>
 
     <div class="mb-3">
         <label for="img2" class="form-label">Imagen de producto 2: </label>
-        <input type="file" class="form-control" id="img2" name="imagen_2">
+        <input type="file" class="form-control" id="imagen2" name="imagen_2">
     </div>
 
     <div class="mb-3">
         <label for="img3" class="form-label">Imagen de producto 3: </label>
-        <input type="file" class="form-control" id="img3" name="imagen_3">
+        <input type="file" class="form-control" id="imagen3" name="imagen_3">
     </div>
 
     <div class="mb-3">
         <label for="img4" class="form-label">Imagen de producto 4: </label>
-        <input type="file" class="form-control" id="img4" name="imagen_4">
+        <input type="file" class="form-control" id="imagen4" name="imagen_4">
     </div>
 
     <div class="mb-3">
-        <label for="idCat" class="form-label">ID categoria: </label>
-        <input type="number" class="form-control" id="idCat" placeholder="ID categoria*" name="idCategoria">
+        <label for="idCategoria" class="form-label">ID categoria: </label>
+        <input type="number" class="form-control" id="idCategoria" placeholder="ID categoria*" name="idCategoria">
     </div>
 
     <div class="mb-3">
-        <label for="idPro" class="form-label">ID proveedor: </label>
-        <input type="number" class="form-control" id="idPro" placeholder="ID proveedor*" name="idProveedor">
+        <label for="idProveedor" class="form-label">ID proveedor: </label>
+        <input type="number" class="form-control" id="idProveedor" placeholder="ID proveedor*" name="idProveedor">
     </div>
     <center>
         <button type="button" onclick="insertar_producto()" class="btn btn-danger">Insertar</button>
+
         <button type="submit" class="btn btn-danger">Registrar</button>
         <button type="reset" class="btn btn-dark">Cancelar</button>
     </center>
--- views/js/function_producto.js
+++ views/js/function_producto.js
@@ -1,6 +1,16 @@
 async function insertar_producto() {
     let codigo = document.getElementById('codigo').value;
     let nombre = document.querySelector('#nombre').value;
+    let detalle = document.querySelector('#detalle').value;
+    let precio = document.querySelector('#precio').value;
+    let stock = document.querySelector('#stock').value;
+    let fechaVencimiento = document.querySelector('#fechaVencimiento').value;
+    let imagen1 = document.querySelector('#imagen1').value;
+    let imagen2 = document.querySelector('#imagen2').value;
+    let imagen3 = document.querySelector('#imagen3').value;
+    let imagen4 = document.querySelector('#imagen4').value;
+    let idCategoria = document.querySelector('#idCategoria').value;
+    let idProveedor = document.querySelector('#idProveedor').value;
     console.log(codigo);
     console.log(nombre);
 }
