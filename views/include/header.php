288cfdd9c468ffe2b049c999af880b60e1ec636e
roliberyucra <yucracuroroliber@gmail.com>
2024-10-17 12:46:11 -0500

    23567

--- views/include/header.php
+++ views/include/header.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/views/plantilla/css/style.css">
     <link rel="icon" href="<?php echo BASE_URL; ?>/views/plantilla/img/loguito.png" type="image/x-icon">
     <script>
-        const base_url = <?php echo BASE_URL; ?>
+        const base_url = '<?php echo BASE_URL; ?>';
     </script>
 </head>
 <body class="p-0 m-0 border-0 bd-example m-0 border-0">
