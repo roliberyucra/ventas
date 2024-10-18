68c3ee8eb84463a21c17e8894685d20471bea3b4
roliberyucra <yucracuroroliber@gmail.com>
2024-10-17 20:14:32 -0500

    123456789

--- views/include/header.php
+++ views/include/header.php
@@ -26,24 +26,27 @@
                     <a href="../index.html"><img src="./views/plantilla/img/letralogo.png" alt="localizacion" width="140px"></a>
                 </div>
 
-                <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                    <div class="container-fluid">
-                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                            <ul class="navbar-nav">
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown link
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="#">Action</a></li>
-                                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
+                <div class="bloque_menu">
+                <div class="bloque_sesion">
+                    <div class="dropdown-center" style="background-color: black;">
+                        <button id="buton_menu" class="btn btn-secondary dropdown-toggle" style="background-color: black; border-color: black;" type="button" data-bs-toggle="dropdown" aria-expanded="false"><font style="vertical-align: inherit; background-color: black;"><font style="vertical-align: inherit; background-color: black;">
+                            Hola, Roliber
+                        </font></font></button>
+                        <ul class="dropdown-menu" style="background-color: black; border-color: grey;">
+                          <li><a class="dropdown-item" href="perfil.html"><font style="vertical-align: inherit; color: white;"><font style="vertical-align: inherit;">Mi cuenta</font></font></a></li>
+                          <li><a class="dropdown-item" href="login.html"><font style="vertical-align: inherit; color: white;"><font style="vertical-align: inherit;">Cerrar sesión</font></font></a></li>
+                        </ul>
                     </div>
-                </nav>
+                </div>
+                <div class="bloque_informacion">
+                    <div class="bloque_info_contactos">
+                        <a href="contactos.html"><img src="./views/plantilla/img/contactos.png" alt="localizacion" width="60px"></a>
+                    </div>
+                    <div class="bloque_info_carrito">
+                        <a href="carrito.html"><img src="./views/plantilla/img/carrito.png" alt="localizacion" width="40px"></a>
+                    </div>
+                </div>
+            </div>
 
             </div>
             <div class="bloque_menu">
