94056a86109d3ef67bf8d4f2b4e6a4b04e39adfe
roliberyucra <yucracuroroliber@gmail.com>
2024-10-22 09:17:28 -0500

    connect_bd

--- librerias/conexion.php
+++ librerias/conexion.php
@@ -2,6 +2,15 @@
     require_once "./config/config.php";
 
     class conexion{
+        public static function connect(){
+           // $mysql = new mysqli(BD_HOST, BD_NAME, BD_USER, BD_PASSWORD);
+           $mysql = new mysqli(BD_HOST, BD_USER, BD_PASSWORD, BD_NAME);
+           $mysql->set_charset(BD_CHARSET);
+           if (mysqli_connect_errno()) {
+                echo "Error de conexión: ".mysqli_connect_errno();
+           }
+           return $mysql;
+        }
 
     }
 
