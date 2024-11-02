c6099cfa53232f2b9599e4afd07706fa32840a2d
roliberyucra <yucracuroroliber@gmail.com>
2024-11-02 10:52:14 -0500

    rqwertyu

--- views/include/footer.php
+++ views/include/footer.php
@@ -51,6 +51,11 @@
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <!-- Fin Script Mensaje de alter (Anibal) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script
+     src="https://code.jquery.com/jquery-3.7.1.js"
+     integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+     crossorigin="anonymous"></script>
 </body>
     
 </html>
