3d1e41195e664329a4cd7e525c766d447c652f16
roliberyucra <yucracuroroliber@gmail.com>
2024-11-28 08:39:41 -0500

    enlaces-admin

--- views/panel-administracion.php
+++ views/panel-administracion.php
@@ -3,35 +3,35 @@
         <center><h3><b>BIENVENIDO al Panel de Administración</b></h3></center>
     <center>
         <br>
-        <a href="<?php echo BASE_URL; ?>/view-categoria" class="btn btn-danger">Ver categorias</a>
+        <a href="<?php echo BASE_URL; ?>/view-categoria-admin" class="btn btn-danger">Ver categorias</a>
         <a href="<?php echo BASE_URL; ?>/insert-categoria" class="btn btn-light">Registrar categorias</a>
         <br>
         <br>
-        <a href="<?php echo BASE_URL; ?>/view-compra" class="btn btn-danger">Ver compras</a>
+        <a href="<?php echo BASE_URL; ?>/view-compra-admin" class="btn btn-danger">Ver compras</a>
         <a href="<?php echo BASE_URL; ?>/insert-compra" class="btn btn-light">Registrar compra</a>
         <br>
         <br>
-        <a href="<?php echo BASE_URL; ?>/view-venta" class="btn btn-danger">Ver detalle de ventas</a>
+        <a href="<?php echo BASE_URL; ?>/view-venta-admin" class="btn btn-danger">Ver detalle de ventas</a>
         <a href="<?php echo BASE_URL; ?>/insert-venta" class="btn btn-light">Registrar venta</a>
         <br>
         <br>
-        <a href="<?php echo BASE_URL; ?>/view-pago" class="btn btn-danger">Ver pagos</a>
+        <a href="<?php echo BASE_URL; ?>/view-pago-admin" class="btn btn-danger">Ver pagos</a>
         <a href="<?php echo BASE_URL; ?>/insert-pago" class="btn btn-light">Registrar pago</a>
         <br>
         <br>
-        <a href="<?php echo BASE_URL; ?>/view-persona" class="btn btn-danger">Ver personas</a>
+        <a href="<?php echo BASE_URL; ?>/view-persona-admin" class="btn btn-danger">Ver personas</a>
         <a href="<?php echo BASE_URL; ?>/insert-persona" class="btn btn-light">Registrar persona</a>
         <br>
         <br>
-        <a href="<?php echo BASE_URL; ?>/view-producto" class="btn btn-danger">Ver productos</a>
+        <a href="<?php echo BASE_URL; ?>/view-producto-admin" class="btn btn-danger">Ver productos</a>
         <a href="<?php echo BASE_URL; ?>/insert-producto" class="btn btn-light">Registrar producto</a>
         <br>
         <br>
-        <a href="<?php echo BASE_URL; ?>/view-sesion" class="btn btn-danger">Ver sesiones</a>
+        <a href="<?php echo BASE_URL; ?>/view-sesion-admin" class="btn btn-danger">Ver sesiones</a>
         <a href="<?php echo BASE_URL; ?>/insert-sesion" class="btn btn-light">Registrar sesión</a>
         <br>
         <br>
-        <a href="<?php echo BASE_URL; ?>/view-venta" class="btn btn-danger">Ver ventas</a>
+        <a href="<?php echo BASE_URL; ?>/view-venta-admin" class="btn btn-danger">Ver ventas</a>
         <a href="<?php echo BASE_URL; ?>/insert-venta" class="btn btn-light">Registrar venta</a>
         <br>
     </center>
